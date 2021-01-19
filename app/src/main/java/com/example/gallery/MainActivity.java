80bb44c689d7359b612f74a1e55493787d0c8f59
neopetz <61999023+neopetz@users.noreply.github.com>
2021-01-19 15:17:47 +0800

    add bbbbb

--- app/src/main/java/com/example/gallery/MainActivity.java
+++ app/src/main/java/com/example/gallery/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 3000;
     LazyLoader lazyLoader;
 //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    //bbbbbbbbbbbbbbbbbb
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
